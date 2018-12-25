bfbc9cc702dbc41d4ea695d4c08be3e9c42876ad
Evgenii Kozlov1 <Evgenii_Kozlov1@epam.com>
2018-12-25 17:25:38 +0400

    variables added

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,2 @@
 'use strict';
 import './img.js';
-import CalcInit  from './calculator/calculator.js';
-
-CalcInit('calculator1');
-CalcInit('calculator2');
--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,17 @@
 const path              = require('path');
 const ExtractTextPlugin = require ('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-let   faviconsWebpack   = require('favicons-webpack-plugin');
-var   sass              = require('sass');
-var   postcss    		= require('postcss');
+const sass              = require('sass');
+const postcss    		= require('postcss');
 const CONSTANTS         = require("./constants");
 const AUTOPREFIXER      = require('autoprefixer');
 const EVENT 			= process.env.npm_lifecycle_event || '';
 const PROD 				= EVENT.includes('prod');
 const DEV 				= EVENT.includes('dev');
-var   pathToDist 		= PROD ? ('dist/prod') : ('dist/dev');
-var   confMode 			= PROD ? (CONSTANTS.WEBPACK_MODE_PROD) : (CONSTANTS.WEBPACK_MODE_DEV);
-var   postCssLoader 	= PROD ? ({plugins: [AUTOPREFIXER({browsers:['ie >= 8', 'last 12 version']})],sourceMap: false}) : ({sourceMap: true});
+const pathToDist 		= PROD ? ('dist/prod') : ('dist/dev');
+const postCssLoader 	= PROD ? ({plugins: [AUTOPREFIXER({browsers:['ie >= 8', 'last 12 version']})],sourceMap: false}) : ({sourceMap: true});
 
-/*CONFIG*/
+/*CONFIG*/				
 
 var clientConfig = (function webpackConfig() {
   var config = Object.assign({});
@@ -26,9 +24,9 @@ var clientConfig = (function webpackConfig() {
   config.module = {
     rules: [
 			{test: /\.js$/,
-				exclude: /node_modules/,
+			/*	exclude: /node_modules/,
 				use: {loader: 'babel-loader',
-				options: {presets: ['babel-preset-env']}}
+				options: {presets: ['babel-preset-env']}} */
 			},
 			{test: /\.(sa|sc|c)ss$/,
 				include: path.resolve(__dirname, 'src/scss'),
@@ -48,13 +46,13 @@ var clientConfig = (function webpackConfig() {
 					options: {name: './img/[name].[ext]',
 					context: path.resolve(__dirname, "./img")}
 					}]
-			}/*,
+			},
 			{test: /\.(ttf|eot|woff|woff2)$/,
 				use:
 					{loader: "file-loader",
 					options: {name: "[name].[ext]",
 					context: path.resolve(__dirname, "./fonts")}}
-			}*/
+			}
 		]
   }
 	config.resolve = {};
@@ -71,35 +69,13 @@ var clientConfig = (function webpackConfig() {
 		  "chunks": {"head": {"entry": "",  "css": [ "css/index.css" ]},
                  "main": {"entry": "js/main.js", "css": []}},
 	    },
-	    title: CONSTANTS.HTML_TITLE,
-	    author: CONSTANTS.HTML_AUTHOR,
-		mode: confMode,
 			
 	    minify: {collapseWhitespace: true},
 	    filename: 'index.html',
 	    template: './src/html/template/index.html'
         }),
-	  new faviconsWebpack({
-        logo: './src/img/favicon/favicon.jpg',
-        prefix: 'img/favicon/',    
-        emitStats: false,
-        statsFilename: 'iconstats-[hash].json',
-        persistentCache: true,
-        inject: true,
-	    background: '#fff',
-        title: 'Webpack App',
-	    icons: {android: true,appleIcon: false,appleStartup: false,coast: false,favicons: true,
-                firefox: true, opengraph: false,twitter: false,yandex: false,windows: true}
-	  })
+
 	]
-	/*config.plugins.push(
-		new Plugin()
-	)
-	config.plugins.push(
-		new Plugin()
-	)*/
-
-
 return config;
 });
 module.exports = clientConfig;
